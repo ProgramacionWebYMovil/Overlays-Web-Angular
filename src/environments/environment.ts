b657d35bda2c0a300d2e69fdf12b0317145a1888
KenaiGonzalezOrtega <113781990+KenaiGonzalezOrtega@users.noreply.github.com>
2023-03-29 13:05:08 +0100

    Creado proyecto angular y configuración inicial del firebase.

---
+++ src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  firebase: {
+    projectId: 'streamingoverlays-3f50d',
+    appId: '1:41682838097:web:779a9ea11636d896e24e70',
+    databaseURL: 'https://streamingoverlays-3f50d-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'streamingoverlays-3f50d.appspot.com',
+    apiKey: 'AIzaSyAsuH7-qkHfQ6URnLkaFJaA74zVtIY3JKE',
+    authDomain: 'streamingoverlays-3f50d.firebaseapp.com',
+    messagingSenderId: '41682838097',
+    measurementId: 'G-CV3M01YLZG',
+  },
+  
+}
