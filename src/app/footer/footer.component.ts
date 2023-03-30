30063dfb1c4ab821463c99750caa8d4e6ff560a9
jpere <jpereiro1@gmail.com>
2023-03-30 19:01:34 +0100

    Css y html terminados del footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.css']
 })
 export class FooterComponent {
-  
+
+  //Explore section
+  title_explore: String = "Explore";
+  text_explore1: String = "prueba";
+  text_explore2: String = "prueba";
+  text_explore3: String = "prueba";
+  text_explore4: String = "prueba";
+
+  //Follow section
+  title_follow: String = "Follow";
+  text_instagram: String = "prueba";
+  text_youtube: String = "prueba";
+  text_tiktok: String = "prueba";
+  text_facebook: String = "prueba";
+
+  //Form section
+  title_form: String = "Form";
+  text_email: String = "email";
+  text_name: String = "name";
+  text_message: String = "message";
+
+  //Copyright section
+  text_copyright1: String = "Privac";
+  text_copyright2: String = "Privac";
+  text_copyright3: String = "Privac";
+
 }
