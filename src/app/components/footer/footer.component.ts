e1fd65e832570dbff8ffb625711ba583ec671244
jpere <jpereiro1@gmail.com>
2023-04-01 19:38:43 +0100

    Finalización de la carga de datos en el footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -10,46 +10,26 @@ import { FirestoreService } from 'src/app/services/firestore/firestore.service';
 export class FooterComponent {
 
 
-  pepe = {
+  pageContent = {
     title_explore:"",
     text_overlays:"",
-  text_howToUse:"",
-  text_home:"",
-  text_pricing:"",
+    text_howToUse:"",
+    text_home:"",
+    text_pricing:"",
 
-  //Follow section
-  title_follow:"",
-  text_instagram:"",
-  text_youtube:"",
-  text_tiktok:"",
-  text_facebook:"",
+    //Follow section
+    title_follow:"",
+    text_instagram:"",
+    text_youtube:"",
+    text_tiktok:"",
+    text_facebook:"",
 
-  //Form section
-  title_form:"",
-  text_email:"",
-  text_name:"",
-  text_message:""
+    //Form section
+    title_form:"",
+    text_email:"",
+    text_name:"",
+    text_message:""
   }
-
-  //Explore section
-  /*title_explore: string = "";
-  text_explore1: string = "";
-  text_explore2: string = "";
-  text_explore3: string = "";
-  text_explore4: string = "";
-
-  //Follow section
-  title_follow: string = "";
-  text_instagram: string = "";
-  text_youtube: string = "";
-  text_tiktok: string = "";
-  text_facebook: string = "";
-
-  //Form section
-  title_form: string = "";
-  text_email: string = "";
-  text_name: string = "";
-  text_message: string = "";*/
 
   constructor (private db:FirestoreService){}
 
@@ -63,12 +43,8 @@ export class FooterComponent {
 
   private setTextData(data: object) {
 
-      for (const [key,value] of Object.entries(data)){
-        console.log(value,key);
-        this.pepe = value; 
+      for (let [key,value] of Object.entries(data)){
+        this.pageContent[key as keyof typeof this.pageContent]=value;
       }
-      console.log(typeof data)
-      this.pepe!=data;
-      
   }
 }
