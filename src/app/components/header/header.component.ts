d38f31551ef35bf82218d398048113fa16651611
SaraES2000 <saraes2000@gmail.com>
2023-04-21 12:42:39 +0100

    selector perfecto, cambios en funciones de change_language y demas

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { User } from '@angular/fire/auth';
 import { Observable } from 'rxjs';
 import { Header } from 'src/app/interfaces/pagesContents.interface';
 import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
@@ -24,8 +23,6 @@ export class HeaderComponent {
     { value: 'en', label: 'English' }
   ];
 
-  //selectedLanguage: string = this.languages[0].value;
-  //selectedLanguage: string | null = null;
   selectedLanguage: string = '';
 
 
@@ -40,23 +37,13 @@ export class HeaderComponent {
     this.load.loadContent('header').then(data => {
       this.pageContent = data;
     });
-    const selectedLanguage = localStorage.getItem('language');
-    if (selectedLanguage !== null) {
-      this.selectedLanguage = selectedLanguage;
-    }
+    this.selectedLanguage = 'null';
   }
-
-
 
   async logOut(){
     await this.auth.logOut();
   }
 
-  /*
-  changeLanguage(){
-    this.load.changeLanguage();
-    window.location.reload();
-  }*/
   changeLanguage(){
     if(this.selectedLanguage === 'es'){
       this.load.setCurrentLanguage(1);
@@ -65,9 +52,6 @@ export class HeaderComponent {
     }
     window.location.reload();
   }
-
-
-
 
 }
 
