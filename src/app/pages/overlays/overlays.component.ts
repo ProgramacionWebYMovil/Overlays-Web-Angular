9716134358cda55147d3fe727695cd79ee62a0e7
SaraES2000 <saraes2000@gmail.com>
2023-04-24 02:26:38 +0100

    estilos mejorados

--- src/app/pages/overlays/overlays.component.ts
+++ src/app/pages/overlays/overlays.component.ts
@@ -17,15 +17,13 @@ export class OverlaysComponent {
 
   constructor(public loadContent:OverlaysService){ }
 
-  /*ngOnInit(){
-
+  ngOnInit(){
     this.loadContent.loadDemoOverlays().then(data => {
       this.overlays = this.loadContent.fillOverlays(0,this.loadContent.getCardsPerPage());
       this.paginationReady = true;
     });
-
   }
-*/
+
   //METODO LLAMADO POR APP-PAGINATION
   changePage(startEnd:{start:number,end:number}):void{
     this.loadContent.fillOverlays(startEnd.start,startEnd.end);
