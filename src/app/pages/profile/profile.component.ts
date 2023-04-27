1907d358d6f3fa797ce36d24a21c45a254ee33a9
SaraES2000 <saraes2000@gmail.com>
2023-04-27 16:35:14 +0100

    perfil

---
+++ src/app/pages/profile/profile.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent {
+  selectedImage: any;
+  defaultValue: any = 'Valor predeterminado';
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.selectedImage = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+}
